b1ffa15466cdb87d0ae2364449f4ca8170351f24
nicky4gun <nicklas.stagesorensen@outlook.dk>
2026-01-29 11:02:27 +0100

    Initial commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module com.example.tidsrejseagenturet {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens com.example.tidsrejseagenturet to javafx.fxml;
+    exports com.example.tidsrejseagenturet;
+}
